7f8823b226ed0cfd2cf6c8fc5a8ead54fac3972a
Reza Goodarzi <reza.2002.goodarzi@gmail.com>
2023-11-11 22:28:06 +0330

    exercise 1

--- index.php
+++ index.php
@@ -1,75 +1,15 @@
-<!-- <!DOCTYPE html>
-<html>
-
-<head>
-</head>
-
-<body>
-
-    <form method = "POST" action = "
-
-
-</body>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-</head>
-
-<body>
-
-<form  method = "post" action =" <?php $_SERVER["PHP_SELF"] ?> " >
-<label> Number: </label>
-<input type = "text" name = "number">
-<br><br>
-<input type = "submit">
 <?php
 
 
-if(empty($_POST['number'])){    //شماره خالی باشد
-    echo "its empty";
-}
-$num = strlen($_POST['number']);
 
-if($num != 11){     //شماره بیشتر از 11
-        echo "error";
+function seconds($m){
+
+    if($m == true){
+        $m = $m * 60;
     }
-if(substr($_POST['number'], 0, 2) == "+98"){    //شماره با +98
-
-    echo "dont enter +98";
-    die;
+    return $m;
 }
 
-echo $_SERVER["PHP_SELF"];
-?>
-</body>
-</html>
-
-
-</html> -->
-
-<?php
-
-$arr = array();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo seconds(2);
 
 ?>
